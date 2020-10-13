aaa275bee9b1d20fd5e31418b7735a550ba3344c
Carlos Garcia <cgmilan@gmail.com>
2020-10-12 21:51:27 -0400

    Project component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Hero from '../components/Hero'
-import About from '../components/About'
+import React from 'react';
+import Layout from '../components/Layout';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Project from '../components/Project';
 
 export default ({ data }) => {
     return (
         <Layout>
             <Hero content={data.hero.edges} />
             <About content={data.about.edges} />
+            <Project content={data.project.edges} />
         </Layout>
     );
 };
@@ -43,6 +45,34 @@ export const pageQuery = graphql`
                 }
             }
         }
-
+        project: allMdx(
+            filter: {
+                fileAbsolutePath: { regex: "/project/" } 
+                frontmatter: { visible: { eq: "true" } }
+            }
+            sort: { fields: [frontmatter___position], order: ASC }
+        ) {
+            edges {
+                node {
+                    body
+                    frontmatter {
+                        title
+                        visible
+                        tags
+                        position
+                        github
+                        external
+                        category
+                        screenshot {
+                            childImageSharp {
+                                fluid {
+                                    ...GatsbyImageSharpFluid
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 `
